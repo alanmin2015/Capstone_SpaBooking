8f62128f4f048fea972ada5b1317690cb12265a5
Alan Min <84518355+alanmin2015@users.noreply.github.com>
2023-06-23 20:15:19 -0400

    account and login update

--- spa-reservation-system/src/components/pages/Account.jsx
+++ spa-reservation-system/src/components/pages/Account.jsx
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import bcrypt from 'bcryptjs';
+import './Account.css';
+import { AuthContext } from '../../AuthContext'; 
+import { useParams } from 'react-router-dom';
 
 const Account = () => {
   const [password, setPassword] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [user, setUser] = useState(null); // New state to store user data 
+  const [user, setUser] = useState(null);
   const [showConfirmation, setShowConfirmation] = useState(false);
+  
+  const { userId } = useContext(AuthContext); // Get userId from AuthContext
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const res = await axios.get('http://127.0.0.1:5000/api/users');
-        const users = res.data;
-        if (users.length > 0) {
-          const user = users[0];
-          setUser(user);
-          setPassword(user.password);
-          setFirstName(user.first);
-          setLastName(user.last);
-        }
+        const res = await axios.get(`http://127.0.0.1:5000/api/users/${userId}`);
+        setUser(res.data);
+        setPassword(res.data.password);
+        setFirstName(res.data.first);
+        setLastName(res.data.last);
       } catch (error) {
         console.error('Error fetching user data: ', error);
       }
     };
 
     fetchUserData();
-  }, []);
+  }, [userId]);
 
   const handleUpdate = async () => {
     try {
-      await axios.put('http://127.0.0.1:5000/api/users', {
-        email: user.email,
-        password,
+      
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await axios.put(`http://127.0.0.1:5000/api/users/${userId}`, {
+        password: hashedPassword,
         first: firstName,
         last: lastName
       });
-      toast.success('Account updated successfully');;
+
+      toast.success('Account updated successfully');
     } catch (error) {
       console.error('Error updating account: ', error);
       toast.error('Error updating account');
@@ -48,11 +53,9 @@ const Account = () => {
     setShowConfirmation(true);
   };
 
-  const confirmDelete  = async () => {
+  const confirmDelete = async () => {
     try {
-      await axios.delete('http://127.0.0.1:5000/api/users', {
-        data: { email: user.email }
-      });
+      await axios.delete(`http://127.0.0.1:5000/api/users/${userId}`);
       toast.success('Account deleted successfully');
       setTimeout(() => {
         window.location.href = '/'; // Redirect to login page
@@ -60,8 +63,7 @@ const Account = () => {
     } catch (error) {
       console.error('Error deleting account: ', error);
       toast.error('Error deleting account');
-    }
-    finally {
+    } finally {
       setShowConfirmation(false);
     }
   };
@@ -70,30 +72,55 @@ const Account = () => {
     setShowConfirmation(false);
   };
 
-  if (!user) {
-    return <div>Loading...</div>; // Add loading state while user data is being fetched
+  if (!userId) {
+    return <div>Loading...</div>;
   }
 
-  
-
   return (
-    <div>
+    <div className="account-container">
       <h2>Account</h2>
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <input placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} />
-      <input placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} />
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
       <button onClick={handleUpdate}>Update Account</button>
       <button onClick={handleDelete}>Delete Account</button>
       {showConfirmation && (
         <div>
           <p>Are you sure you want to delete your account?</p>
-          <button onClick={confirmDelete}>Yes</button>
+          <button className="confirm" onClick={confirmDelete}>
+            Yes
+          </button>
           <button onClick={cancelDelete}>No</button>
         </div>
       )}
-      <ToastContainer /> {/* Add ToastContainer component */}
+      <ToastContainer />
     </div>
   );
-}
+};
 
 export default Account;
